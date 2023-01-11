74f4342317c4b7f86c77fe2993f05a79a7333954
Gaeun Lee <gaeun0800@gmail.com>
2023-01-11 18:41:24 +0900

    react setting 위치 변경: client

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          수정,3002포트 <code>src/App.js</code> and save to reload.
+          수정,3001포트 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>HI</p>
       </header>
     </div>
   );
