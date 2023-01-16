7ff6e071fd3928a77d6fa7004da11e2251ebe674
Gaeun Lee <gaeun0800@gmail.com>
2023-01-17 00:18:16 +0900

    페이지 이동 테스트

---
+++ client/src/components/houses.js
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+
+function LinkTest(){
+        return(
+            <>
+            
+            <div>
+                링크 테스트 성공
+            </div>
+
+            </>
+
+        )
+    
+}
+
+
+export default LinkTest;
