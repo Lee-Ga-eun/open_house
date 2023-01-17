4ca9ad819d01d81417b77ff1bd7378cb852d3bab
Gaeun Lee <gaeun0800@gmail.com>
2023-01-18 01:50:25 +0900

    Home 화면 데이터 받아오기
    Link - useLocation 사용

--- client/src/components/houses.js
+++ client/src/components/houses.js
@@ -1,10 +1,19 @@
 import { render } from "@testing-library/react";
+import Header from "./header";
+import { useLocation } from 'react-router';
+
 
 function LinkTest(){
+
+
+    const location = useLocation();
+    console.log(location);
+
         return(
             <>
-            
+            <Header/>
             <div>
+                <p>{location.state.sido}</p>
                 링크 테스트 성공
             </div>
 
