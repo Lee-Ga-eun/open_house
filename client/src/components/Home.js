e9bb2fecf7e736dbaeea9910404222c0f2b5fd4d
Gaeun Lee <gaeun0800@gmail.com>
2023-01-19 02:22:29 +0900

    select한 값을 넘겨주기 위해 데이터를 정리하기

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,30 +16,72 @@ function Home() {
   const [val2, setVal2] = useState("");
   const [val3, setVal3] = useState("");
   const [sidoForNext, setSidoForNext]=useState("");
+  const [siGuGunForLink, setSiGuGunForLink]=useState("");
+  const [dongForLink, setDongForLink]=useState("");
+  const [housesData, setHousesData] =useState([]);
+
   const { sido, sigugun, dong } = hangjungdong;
-  console.log("sido",sido);
-
   const onChange=(e)=>{
-    setVal1(e.target.value);
-    console.log("e.target.value",e.target.value);
-    // 업데이트된 val1값은 11.
-    // {sido:'11' , codeNm:"서울특별시"}에서 서울특별시를 빼오고 싶다
-    // Object 이름은 sido
-    const findName= sido.map(i => [i.sido, i.codeNm]);
-    console.log("findName", findName);
-    console.log(findName);
-    console.log(sido);
-    //const onlyName= sido.map(i=> i.codeNm ? i.sido==e.target.value : "");
-    const onlyName= sido.find(i=> i.sido==e.target.value ? i.codeNm : "");
-    console.log(onlyName.codeNm);
 
-    console.log("onlyName",onlyName);
+    // 시도 
+    const sidoName = e.target.id=="sido" ? sido.find(i=> i.sido==e.target.value ? i.codeNm : "").codeNm : "";
+    console.log(sidoName);
+    const sidoSetChange=()=> sidoName!="" ? setVal1(e.target.value) : "";
+    sidoSetChange();
+    //setSidoForNext(sidoName);
+    const sidoValueChange= e.target.id=="sido" ? setSidoForNext(sidoName) : "";
+    
+    //시군구
+    const sigunGuName = e.target.id=="sigungu" ? sigugun.find(i => i.sido==val1 && i.sigugun==e.target.value ? i.codeNm : "").codeNm  :"";
+    console.log("sigunguname",sigunGuName);
+    const sigunguValue = e.target.id=="sigungu" ? setSiGuGunForLink(sigunGuName): ""; // 렌더링되면서 결국 마지막 setDongForLink만 적용되는 문제를 삼항연산자로 해결
+    console.log("sigunguforlink",siGuGunForLink);
 
-    setSidoForNext(onlyName.codeNm);
-    //console.log("e.target.name",e.target.name);
+    const sigunguSetChange=()=> sigunGuName!="" ? setVal2(e.target.value) : "";
+    sigunguSetChange();
 
-  }
 
+    //동
+    const dongName = e.target.id=="dong" ? dong.find(i => (i.sido===val1) && (i.sigugun === val2) && (i.dong===e.target.value) ? i.codeNm : "").codeNm : "";
+    console.log("dongName",dongName);
+    setDongForLink(dongName);
+    console.log("dongForLink",dongForLink);
+
+    const dongSetChange = () => dongForLink!=="" ? setVal3(e.target.value) : "";
+    dongSetChange();
+
+
+
+    // console.log("e.target.value",e.target.value);
+    // // 업데이트된 val1값은 11.
+    // // {sido:'11' , codeNm:"서울특별시"}에서 서울특별시를 빼오고 싶다
+    // // Object 이름은 sido
+    // const findName= sido.map(i => [i.sido, i.codeNm]);
+    // console.log("findName", findName);
+    // console.log(findName);
+    // console.log(sido);
+    // //const onlyName= sido.map(i=> i.codeNm ? i.sido==e.target.value : "");
+    // const onlyName= sido.find(i=> i.sido==e.target.value ? i.codeNm : "");
+    // console.log(onlyName.codeNm);
+
+    // console.log("onlyName",onlyName);
+
+    // setSidoForNext(onlyName.codeNm);
+    // //console.log("e.target.name",e.target.name);
+
+  };
+  let arr=[]
+
+  const housesPage = (e) =>{
+
+
+    arr=[sidoForNext, siGuGunForLink, dongForLink];
+    console.log(arr);
+    //setHousesData(...housesData, arr);
+    //console.log("housesData",housesData);
+
+
+  };
 
 
   return (
@@ -52,19 +94,11 @@ function Home() {
             <Form.Select aria-label="Disabled select example" disabled className={styles.locationForm}>
             <option>대한민국</option>
             </Form.Select>
-
-            {/* <Form.Select aria-label="Default select example" className={styles.locationForm}>
-            <option>Select location</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </Form.Select> */}
-
             {/*  */}
 
             {/* onChange 이벤트가 발생할 때, 앞으로 변할 e.target.value를 setVal1에 넣어줌으로써 val1값 리렌더링 */}
             {/* <Form.Select className={styles.locationForm} onChange={(e) => setVal1(e.target.value)}> */}
-            <Form.Select className={styles.locationForm} onChange={onChange}>
+            <Form.Select id='sido' className={styles.locationForm} onChange={onChange}>
               <option value="">시/도</option>
               {sido.map((el) => (
                 <option key={el.sido} value={el.sido}>
@@ -73,20 +107,23 @@ function Home() {
               ))}
             </Form.Select>
             {/*  */}
-            {console.log("val1",val1)}
+            {/* {console.log("val1",val1)} */}
 
-            <Form.Select className={styles.locationForm} onChange={(e) => setVal2(e.target.value)}>
+            {/* <Form.Select className={styles.locationForm} onChange={(e) => setVal2(e.target.value)}> */}
+            <Form.Select className={styles.locationForm} onChange={onChange} id="sigungu">
             <option value="">시/군/구</option>
             {sigugun
               .filter((el) => el.sido === val1) // 수행 전에 시도가 선택되어야 있어야함
               .map((el) => (
-                <option key={el.sigugun} value={el.sigugun}>
+                <option key={el.sigugun} value={el.sigugun} >
                   {el.codeNm}
                 </option>
               ))}
             </Form.Select>
 
-            <Form.Select className={styles.locationForm} onChange={(e) => setVal3(e.target.value)}>
+            {/* <Form.Select className={styles.locationForm} onChange={(e) => setVal3(e.target.value)}> */}
+            <Form.Select className={styles.locationForm} onChange={onChange} id="dong">
+
               <option value="">동</option>
               {dong
                 .filter((el) => el.sido === val1 && el.sigugun === val2) // 시도 & 시군구 모두 선택되어 있어야 함
@@ -97,10 +134,11 @@ function Home() {
                   </option>
                 ))}
       </Form.Select>
+      {console.log(val3)}
 
       {/* 버튼을 누르면 페이지를 이동할 수 있도록 구현한다 */}
-        <Link to="/test" state={{sido:sidoForNext}}>
-          <Button className={styles.submit} variant="dark" onClick={()=>{alert(val1)}}>GO</Button>
+        <Link to="/test" state={{sido:arr}}>
+          <Button className={styles.submit} variant="dark" onClick={housesPage} id="go">GO</Button>
         </Link>    
 
         </div>
@@ -108,7 +146,11 @@ function Home() {
       </div>   
       <br />
       {/* 지역명 출력 */}
-      {sidoForNext}
+      <div>
+        <h1>{sidoForNext!=="" ? sidoForNext : ""}</h1>
+        <h1>{siGuGunForLink!==""? siGuGunForLink:""}</h1>
+        <h1>{dongForLink!==""? dongForLink: ""}</h1>
+      </div>
 
     </>
   );
