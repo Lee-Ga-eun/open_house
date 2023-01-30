0f4d38d1e1b9c32c35954109fd2f58027d5458f8
Gaeun Lee <gaeun0800@gmail.com>
2023-01-31 01:11:41 +0900

    게시글 테이블 생성 + post

--- server.js
+++ server.js
@@ -52,11 +52,28 @@ app.get('/api/houses',(req,res)=>{
     );
 });
 
+// app.post('/api/houses/upload',(req,res)=>{
+//     res.header("Access-Control-Allow-Origin", "*"); // cors 에러 해결
+//     const name=req.body.name;
+//     const sqlQuery="INSERT INTO Test(NAME) VALUES (?)";
+//     connection.query(sqlQuery, [name], (err,result)=>{
+//         res.send('success');
+//     });
+    
+// });
+
 app.post('/api/houses/upload',(req,res)=>{
     res.header("Access-Control-Allow-Origin", "*"); // cors 에러 해결
-    const name=req.body.name;
-    const sqlQuery="INSERT INTO Test(NAME) VALUES (?)";
-    connection.query(sqlQuery, [name], (err,result)=>{
+    const sido=req.body.sido;
+    const author=req.body.author;
+    const sigungu=req.body.sigungu;
+    const dong=req.body.dong;
+    const content=req.body.content;
+    const image=req.body.image;
+    const title=req.body.title;
+
+    const sqlQuery="INSERT INTO Posting(AUTHOR, SIDO, SIGUNGU, DONG, CONTENT, IMAGE,TITLE) VALUES (?,?,?,?,?,?,?)";
+    connection.query(sqlQuery, [author, sido, sigungu,dong,content,image,title], (err,result)=>{
         res.send('success');
     });
     
