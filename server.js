ad82e3cde25b5c787520e9a5cd77d3941a53ee4e
Gaeun Lee <gaeun0800@gmail.com>
2023-02-03 02:08:16 +0900

    for 업로드 기능

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/api/houses',(req,res)=>{
     
 // });
 
-
+/*
 app.post('/api/houses/upload', (req,res)=>{
     res.header("Access-Control-Allow-Origin", "*"); // cors 에러 해결
     const sido=req.body.sido;
@@ -82,7 +82,7 @@ app.post('/api/houses/upload', (req,res)=>{
         res.send('success');
     });
     
-});
+});*/
 
 app.get('/api/houses/upload',(req,res)=>{
     // res.send({message:"DB연결테스트"})
@@ -94,13 +94,35 @@ app.get('/api/houses/upload',(req,res)=>{
     );
 
 });
+
+app.post('/api/houses/upload', upload.single('image'),(req,res,err)=>{
+    res.header("Access-Control-Allow-Origin", "*"); // cors 에러 해결
+    const sido=req.body.sido;
+    const author=req.body.author;
+    const sigungu=req.body.sigungu;
+    const dong=req.body.dong;
+    const content=req.body.content;
+    const title=req.body.title;
+    let image='/image/'+req.file.filename;
+
+    console.log(req.file.filename);
+
+    //let image='http://localhost:5001/image/'+req.file.filename;
+
+    const sqlQuery="INSERT INTO Posting(AUTHOR, SIDO, SIGUNGU, DONG, CONTENT, IMAGE,TITLE) VALUES (?,?,?,?,?,?,?)";
+    connection.query(sqlQuery, [author, sido, sigungu,dong,content,image,title], (err,result)=>{
+        res.send('success');
+    });
+    
+});
+
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use('/image',express.static('./upload'));
 
 app.post('/api/imgtest', upload.single('image'),(req,res,err)=>{
     res.header("Access-Control-Allow-Origin", "*"); // cors 에러 해결
-    let sql= 'INSERT INTO IMGTEST(ID, NAME, profileImage,filename) VALUES(null,?,?,?)';
+    let sql= 'INSERT INTO IMGTEST(ID, NAME, profileImage,filename,fileByteName) VALUES(null,?,?,?,?)';
     //console.log(req.data.name);
     //console.log(req.body.profile_img);
     let image='/image/'+req.file.filename;
@@ -111,7 +133,10 @@ app.post('/api/imgtest', upload.single('image'),(req,res,err)=>{
     //const profile_img=req.file.filename;
     //console.log(profile_img);
     let filename=req.body.realfilename;
-    connection.query(sql, [name,image,filename],(rows, err,result)=>{
+
+    let fileByteName=req.file.filename;
+    console.log("fileByateName",fileByteName);
+    connection.query(sql, [name,image,filename, fileByteName],(rows, err,result)=>{
         res.send(rows)
         if(err){
             console.log("에러발생",err);
@@ -125,6 +150,15 @@ app.get('/api/imgtest',(req,res)=>{
         "SELECT * FROM IMGTEST", (err,rows,files)=>{
             res.send(rows);
             console.log("에러",err);
+        }
+    )
+});
+
+app.get('/api/imgtest/fileByteName',(req,res)=>{
+    res.header("Access-Control-Allow-Origin", "*"); // cors 에러 해결
+    connection.query(
+        "SELECT fileByteName from IMGTEST ORDER BY ID DESC LIMIT 1",(err,rows,files)=>{
+            res.send(rows);
         }
     )
 });
